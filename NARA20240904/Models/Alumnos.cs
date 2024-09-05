2b36771f639f412e57bef2722f8902237e4c2b02
Nestor.Adonay.Rodriguez.Alberto@gmail.com <nestor.adonay.rodriguez.alberto@gmail.com>
2024-09-04 21:00:42 -0600

    Agregar archivos de proyecto.

---
+++ NARA20240904/Models/Alumnos.cs
@@ -0,0 +1,10 @@
+﻿namespace NARA20240904.Models
+{
+    public class Alumnos
+    {
+        // Atributos:
+        public int IdAlumno { get; set; }
+        public string Nombre { get; set; }
+        public string Carrera { get; set; } 
+    }
+}
